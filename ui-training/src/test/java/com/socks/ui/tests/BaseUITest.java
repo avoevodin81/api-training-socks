79de80319aab0b8dfb0ad7ed03a65c2191c126e8
Andrii_Voievodin <avoevodin81@gmail.com>
2022-05-14 19:07:56 +0300

    add allure to ui tests

--- ui-training/src/test/java/com/socks/ui/tests/BaseUITest.java
+++ ui-training/src/test/java/com/socks/ui/tests/BaseUITest.java
@@ -2,7 +2,9 @@ package com.socks.ui.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.socks.api.config.ProjectConfig;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
 import org.testng.annotations.BeforeSuite;
@@ -11,6 +13,14 @@ public class BaseUITest {
 
     @BeforeSuite
     public void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        // or for fine-tuning:
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false)
+        );
+
         ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
         RestAssured.baseURI = config.baseUrl();
 
